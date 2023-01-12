0cd1ee9a6f6511399656140ebdc1590d70ff0788
joaoluccas <joaoluccas66@gmail.com>
2023-01-12 20:54:46 -0300

    Nova página adicionada e algumas correções

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/curriculo')
+
+def curriculo():
+    return render_template('curriculo.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
